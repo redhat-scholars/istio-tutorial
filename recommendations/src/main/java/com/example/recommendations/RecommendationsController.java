404aa9e497144dce18e400023975ce1589148707
burrsutter <burrsutter@gmail.com>
2017-12-23 12:55:26 -0500

    adding misbehave, injecting some bad behavior

--- recommendations/src/main/java/com/example/recommendations/RecommendationsController.java
+++ recommendations/src/main/java/com/example/recommendations/RecommendationsController.java
@@ -4,15 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class RecommendationsController {
+    int cnt = 0;
+    boolean misbehave = false;
     
     @RequestMapping("/")
     public String getRecommendations() {
         
-        System.out.println("Big Red Dog v1");
-
+        cnt ++;
+        System.out.println("Big Red Dog v1 " + cnt);
+        
         /* begin timeout and/or circuit-breaker example
         try {
 			Thread.sleep(3000);
@@ -21,11 +25,25 @@ public class RecommendationsController {
 		}
         System.out.println("recommendations ready to return");
         // end circuit-breaker example */
-        // throw new ServiceUnavailableException();
-        return "Clifford v1";
+        /* inject some poor behavior
+        if (misbehave) {
+            cnt = 0;
+            misbehave = false;
+            throw new ServiceUnavailableException();            
+        } 
+        // */       
+        return "Clifford v1 " + cnt;
+        
     }
 
+    @RequestMapping("/misbehave")
+    public HttpStatus misbehave() {
+        this.misbehave = true;
+        return HttpStatus.OK;
+    }
+    
 }
+
 
 @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
 class ServiceUnavailableException extends RuntimeException {
