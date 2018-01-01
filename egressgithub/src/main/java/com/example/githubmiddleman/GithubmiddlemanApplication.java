6570832e6759a78f401dcf21b06098ac6b0b9eda
burrsutter <burrsutter@gmail.com>
2018-01-01 16:20:00 -0500

    adding egress examples

---
+++ egressgithub/src/main/java/com/example/githubmiddleman/GithubmiddlemanApplication.java
@@ -0,0 +1,12 @@
+package com.example.githubmiddleman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubmiddlemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubmiddlemanApplication.class, args);
+	}
+}
---
+++ egresshttpbin/src/main/java/com/example/httpbinmiddleman/HttpbinmiddlemanApplication.java
@@ -0,0 +1,12 @@
+package com.example.httpbinmiddleman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpbinmiddlemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpbinmiddlemanApplication.class, args);
+	}
+}
