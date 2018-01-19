30422bac760bd15c080b362ce07e5324aa4a984d
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-01-19 22:48:35 +0530

    Throwing Exceptions back to consumer

--- preferences/src/main/java/com/example/preferences/PreferencesController.java
+++ preferences/src/main/java/com/example/preferences/PreferencesController.java
@@ -2,6 +2,7 @@ package com.example.preferences;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,28 +12,30 @@ import org.springframework.http.HttpEntity;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpMethod;
 
+import org.springframework.http.HttpStatus;
+
 @RestController
 public class PreferencesController {
     RestTemplate restTemplate = new RestTemplate();
-    
+
     @RequestMapping("/")
-    public String getPreferences(HttpServletRequest request) {
+    public ResponseEntity<?> getPreferences(HttpServletRequest request) {
         String url = "http://recommendations:8080/";
         // add the tracing headers
         HttpHeaders httpHeaders = new HttpHeaders();
         String headerValue = request.getHeader("x-request-id");
         httpHeaders.add("x-request-id", headerValue);
-        headerValue = request.getHeader("x-b3-traceid");            
+        headerValue = request.getHeader("x-b3-traceid");
         httpHeaders.add("x-b3-traceid", headerValue);
-        headerValue = request.getHeader("x-b3-spanid");            
+        headerValue = request.getHeader("x-b3-spanid");
         httpHeaders.add("x-b3-spanid", headerValue);
-        headerValue = request.getHeader("x-b3-parentspanid");            
+        headerValue = request.getHeader("x-b3-parentspanid");
         httpHeaders.add("x-b3-parentspanid", headerValue);
-        headerValue = request.getHeader("x-b3-sampled");            
+        headerValue = request.getHeader("x-b3-sampled");
         httpHeaders.add("x-b3-sampled", headerValue);
-        headerValue = request.getHeader("x-b3-flags");            
+        headerValue = request.getHeader("x-b3-flags");
         httpHeaders.add("x-b3-flags", headerValue);
-        headerValue = request.getHeader("x-ot-span-context");            
+        headerValue = request.getHeader("x-ot-span-context");
         httpHeaders.add("x-ot-span-context", headerValue);
         // end adding the tracing headers
         // if you wish to create routing rules on user-agent 
@@ -42,20 +45,35 @@ public class PreferencesController {
 
         HttpEntity<String> httpEntity = new HttpEntity<>("", httpHeaders);
 
-        
         String responseBody;
 
         try {
-            ResponseEntity<String> response
-            = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
-            responseBody = response.getBody();
-        } catch (Exception e) {            
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+            return response;
+        } catch (Exception e) {
             responseBody = e.getMessage();
+            System.out.println("Ex:" + responseBody);
+            if (responseBody.contains("java.net.UnknownHostException")
+                    || responseBody.contains("Service Unavailable")) {
+                throw new ServiceUnavailableException(responseBody);
+            } else {
+                throw new ServiceException(responseBody);
+            }
         }
-        
-        System.out.println(responseBody);
-
-
-        return "{\"P1\":\"Red\", \"P2\":\"Big\"} && " + responseBody;
     }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    class ServiceException extends RuntimeException {
+        public ServiceException(String message) {
+            super(message);
+        }
+    }
+
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    class ServiceUnavailableException extends RuntimeException {
+        public ServiceUnavailableException(String message) {
+            super(message);
+        }
+    }
+
 }
