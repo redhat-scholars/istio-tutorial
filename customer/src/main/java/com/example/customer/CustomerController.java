eb5df872344d1167d1ab03a30b3fc7aaafa3f5aa
Edson Yanaga <edson@yanaga.com.br>
2018-01-22 13:01:28 -0500

    Organized imports on Controller files

--- customer/src/main/java/com/example/customer/CustomerController.java
+++ customer/src/main/java/com/example/customer/CustomerController.java
@@ -1,22 +1,11 @@
 package com.example.customer;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-
-// These for adding the tracing headers
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpEntity;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.http.HttpStatus;
-
-import java.net.UnknownHostException;
 
 @RestController
 public class CustomerController {
