c0fcb20573bfc2f43acbc051a4a856271cd06bce
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-01-24 16:45:41 +0530

    * Tested CB * Refactor Removed harcoded url strings

--- preferences/src/main/java/com/example/preferences/PreferencesController.java
+++ preferences/src/main/java/com/example/preferences/PreferencesController.java
@@ -1,5 +1,6 @@
 package com.example.preferences;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class PreferencesController {
 
-    private static final String REMOTE_URL = "http://recommendations:8080";
+    @Value("${recommendations.api.url:http://recommendations:8080}")
+    private String remoteURL;
 
     private static final String RESPONSE_STRING_FORMAT = "{\"P1\":\"Red\", \"P2\":\"Big\"} && %s";
 
@@ -25,11 +27,12 @@ public class PreferencesController {
     @RequestMapping("/")
     public ResponseEntity<?> getPreferences(HttpServletRequest request) {
         try {
-            String responseBody = restTemplate.getForObject(REMOTE_URL, String.class);
+            String responseBody = restTemplate.getForObject(remoteURL, String.class);
             return ResponseEntity.ok(String.format(RESPONSE_STRING_FORMAT, responseBody));
 
         } catch (RestClientException ex) {
-            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(String.format(RESPONSE_STRING_FORMAT, ex.getCause()));
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body(String.format(RESPONSE_STRING_FORMAT, ex.getCause()));
         }
     }
 
