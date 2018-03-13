226f6203becef772b56f5492a9a1da9667443551
Pavol Loffay <ploffay@redhat.com>
2018-03-13 13:28:10 +0100

    Use OpenTracing to propagate tracing headers

--- customer/java/src/main/java/com/redhat/developer/demos/customer/CustomerApplication.java
+++ customer/java/src/main/java/com/redhat/developer/demos/customer/CustomerApplication.java
@@ -1,15 +1,12 @@
 package com.redhat.developer.demos.customer;
 
-import com.redhat.developer.demos.customer.tracing.HttpHeaderForwarderClientHttpRequestInterceptor;
-import com.redhat.developer.demos.customer.tracing.HttpHeaderForwarderHandlerInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.util.Collections;
 
 @SpringBootApplication
 public class CustomerApplication extends WebMvcConfigurerAdapter {
@@ -18,16 +15,8 @@ public class CustomerApplication extends WebMvcConfigurerAdapter {
         SpringApplication.run(CustomerApplication.class, args);
     }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new HttpHeaderForwarderHandlerInterceptor());
-    }
-
     @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setInterceptors(Collections.singletonList(new HttpHeaderForwarderClientHttpRequestInterceptor()));
-        return restTemplate;
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
     }
-
 }
