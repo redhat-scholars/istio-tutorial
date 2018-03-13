0f3ea6345b664f982460d50a19c27b8c96ff9374
Pavol Loffay <ploffay@redhat.com>
2018-03-13 13:28:10 +0100

    Use OpenTracing to propagate tracing headers

--- preference/java/src/main/java/com/redhat/developer/demos/preference/PreferencesApplication.java
+++ preference/java/src/main/java/com/redhat/developer/demos/preference/PreferencesApplication.java
@@ -1,15 +1,17 @@
 package com.redhat.developer.demos.preference;
 
-import com.redhat.developer.demos.preference.tracing.HttpHeaderForwarderClientHttpRequestInterceptor;
-import com.redhat.developer.demos.preference.tracing.HttpHeaderForwarderHandlerInterceptor;
+import com.uber.jaeger.Tracer;
+import com.uber.jaeger.propagation.B3TextMapCodec;
+import com.uber.jaeger.reporters.RemoteReporter;
+import com.uber.jaeger.senders.HttpSender;
+import io.opentracing.propagation.Format.Builtin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.util.Collections;
 
 @SpringBootApplication
 public class PreferencesApplication extends WebMvcConfigurerAdapter {
@@ -18,16 +20,20 @@ public class PreferencesApplication extends WebMvcConfigurerAdapter {
 		SpringApplication.run(PreferencesApplication.class, args);
 	}
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new HttpHeaderForwarderHandlerInterceptor());
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder.build();
 	}
 
 	@Bean
-	public RestTemplate restTemplate() {
-		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.setInterceptors(Collections.singletonList(new HttpHeaderForwarderClientHttpRequestInterceptor()));
-		return restTemplate;
+	public io.opentracing.Tracer tracer() {
+		return new Tracer.Builder("preferences")
+				.withExpandExceptionLogs()
+				.registerExtractor(Builtin.HTTP_HEADERS, new B3TextMapCodec())
+				.registerInjector(Builtin.HTTP_HEADERS, new B3TextMapCodec())
+				.withReporter(new RemoteReporter.Builder()
+						.withSender(new HttpSender("http://jaeger-collector.istio-system.svc:14268/api/traces"))
+						.build())
+				.build();
 	}
-
 }
