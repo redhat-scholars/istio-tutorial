9726fd56fb01deea7133b11b45b10fe43758d0d7
Rafael Benevides <rafabene@gmail.com>
2018-04-13 11:53:04 -0300

    Changed the name of the package

--- recommendation/java/microprofile/src/main/java/com/redhat/developer/demos/recommendation/rest/RecommendationApplication.java
+++ recommendation/java/microprofile/src/main/java/com/redhat/developer/demos/recommendation/rest/RecommendationApplication.java
@@ -1,4 +1,4 @@
-package com.redhat.developer.demos.preference.rest;
+package com.redhat.developer.demos.recommendation.rest;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
--- recommendation/java/microprofile/src/main/java/com/redhat/developer/demos/recommendation/rest/RecommendationEndpoint.java
+++ recommendation/java/microprofile/src/main/java/com/redhat/developer/demos/recommendation/rest/RecommendationEndpoint.java
@@ -1,4 +1,4 @@
-package com.redhat.developer.demos.preference.rest;
+package com.redhat.developer.demos.recommendation.rest;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
