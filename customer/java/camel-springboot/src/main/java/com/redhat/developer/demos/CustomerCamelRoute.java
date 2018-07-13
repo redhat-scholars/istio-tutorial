18abbb98a2d5e826163172e45965c8b22088bff0
Aurélien Pupier <apupier@redhat.com>
2018-07-13 09:49:53 +0200

    remove unused import
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- customer/java/camel-springboot/src/main/java/com/redhat/developer/demos/CustomerCamelRoute.java
+++ customer/java/camel-springboot/src/main/java/com/redhat/developer/demos/CustomerCamelRoute.java
@@ -1,20 +1,13 @@
 package com.redhat.developer.demos;
 
-import io.opentracing.Span;
-import io.opentracing.Tracer;
 import org.apache.camel.Exchange;
-import org.apache.camel.Message;
-import org.apache.camel.builder.ExpressionBuilder;
-import org.apache.camel.builder.PredicateBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.http.common.HttpOperationFailedException;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.apache.camel.opentracing.ActiveSpanManager;
-import org.apache.http.conn.ConnectTimeoutException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
+import io.opentracing.Span;
 
 @Component("CustomerCamelRoute")
 public class CustomerCamelRoute extends RouteBuilder {
