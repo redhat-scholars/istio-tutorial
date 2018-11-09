3204ba3a724d111fd9b0d72d3c74138944ef4791
Alex Soto <asotobu@gmail.com>
2018-11-09 11:20:25 +0100

    Adds micronaut

---
+++ preference/java/micronaut/src/main/java/com/redhat/developer/demos/preference/Application.java
@@ -0,0 +1,10 @@
+package com.redhat.developer.demos.preference;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
