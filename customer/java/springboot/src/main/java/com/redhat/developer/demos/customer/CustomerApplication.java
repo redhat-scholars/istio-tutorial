d4f3ac7fb6d8596a90d5ac7fcbe2f7fb3d6a4c2c
Ioannis Canellos <iocanel@gmail.com>
2019-02-23 22:08:15 +0200

    feat: automatically generate manifests using ap4k.

--- customer/java/springboot/src/main/java/com/redhat/developer/demos/customer/CustomerApplication.java
+++ customer/java/springboot/src/main/java/com/redhat/developer/demos/customer/CustomerApplication.java
@@ -1,5 +1,9 @@
 package com.redhat.developer.demos.customer;
 
+import io.ap4k.docker.annotation.EnableDockerBuild;
+import io.ap4k.kubernetes.annotation.KubernetesApplication;
+import io.ap4k.option.annotation.JvmOptions;
+import io.ap4k.option.annotation.SecureRandomSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -7,7 +11,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-
+@KubernetesApplication(group = "example", version = "v1")
+@EnableDockerBuild
+@JvmOptions(xms=128, xmx=256, preferIPv4Stack = true, secureRandom = SecureRandomSource.NonBlocking)
 @SpringBootApplication
 public class CustomerApplication extends WebMvcConfigurerAdapter {
 
