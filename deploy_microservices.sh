59df0d4b58e960ce7c82b9a70b7c230fca5370ab
Mauricio Leal <mauricio@redbook.local>
2020-04-10 18:20:28 -0300

    Created a Route Customer after deployment

--- deploy_microservices.sh
+++ deploy_microservices.sh
@@ -1,1 +1,2 @@
 for microservice in customer preference recommendation; do oc apply --filename ${microservice}/kubernetes/Deployment.yml; oc apply --filename ${microservice}/kubernetes/Service.yml; done
+oc expose service/customer
