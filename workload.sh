c37310741a801e941dd7cdaeeab7324e44054f44
Mauricio Leal <mauricio@redbook.local>
2020-04-10 18:37:12 -0300

    Make a infinite loop

--- workload.sh
+++ workload.sh
@@ -1,1 +1,1 @@
-while; do curl $(oc get route/customer --output jsonpath='{.spec.port.targetPort}://{.spec.host}'); done
+while true; do curl $(oc get route/customer --output jsonpath='{.spec.port.targetPort}://{.spec.host}'); done
