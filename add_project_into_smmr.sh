377081434087f6d44a90958de4415292d0cded73
Mauricio Leal <mauricio@redbook.local>
2020-04-10 18:08:58 -0300

    Added project into smmr

---
+++ add_project_into_smmr.sh
@@ -0,0 +1,1 @@
+eval "oc patch servicemeshmemberroll/default --type=json --patch '[{\"op\":\"add\",\"path\":\"/spec/members/0\",\"value\":\"$(oc config view --minify --output 'jsonpath={..namespace}')\"}]' --namespace istio-system"
