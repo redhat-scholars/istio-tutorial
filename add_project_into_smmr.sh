506acdfd2067cf65f14e6a3966080cca203984e5
Mauricio Leal <mauricio@redbook.local>
2020-04-21 17:45:48 -0300

    Delete the existing limitrange

--- add_project_into_smmr.sh
+++ add_project_into_smmr.sh
@@ -1,1 +1,2 @@
 eval "oc patch servicemeshmemberroll/default --type=json --patch '[{\"op\":\"add\",\"path\":\"/spec/members/0\",\"value\":\"$(oc config view --minify --output 'jsonpath={..namespace}')\"}]' --namespace istio-system"
+oc delete limitrange/$(oc config view --minify --output 'jsonpath={..namespace}')-core-resource-limits
