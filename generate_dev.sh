f19a0ba82f4f48d6f89796cf96490032020d4382
Alex Soto <asotobu@gmail.com>
2020-04-30 13:01:40 +0200

    -Adds common minikube instructions

--- generate_dev.sh
+++ generate_dev.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-docker run -v `pwd`:/antora --rm -t antora/antora:2.0.0 --pull --stacktrace dev-site.yml
-open gh-pages/istio-tutorial/1.3.x/index.html
+docker run -v `pwd`:/antora --rm -t antora/antora:2.2.0 --pull --stacktrace dev-site.yml
--- generate_workshop.sh
+++ generate_workshop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run -v `pwd`:/antora --rm -t antora/antora:2.0.0 --pull --stacktrace site-workshop.yml
+docker run -v `pwd`:/antora --rm -t antora/antora:2.2.0 --pull --stacktrace site-workshop.yml
 open gh-pages/workshop/istio-tutorial/1.1.x/index.html
