3c6d53a6212fc47c34f978fa9b31edf8b996fdb9
Alex Soto <asotobu@gmail.com>
2020-05-13 08:12:24 +0200

    Fixes timeout instructions

--- masterclassscripts/5_3_resiliency_slow.sh
+++ masterclassscripts/5_3_resiliency_slow.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-kubectl patch deployment recommendation-v2 -p '{"spec":{"template":{"spec":{"containers":[{"name":"recommendation", "image":"quay.io/rhdevelopers/istio-tutorial-recommendation:v2-timeout"}]}}}}' -n tutorial
+kubectl apply -f ./../recommendation/kubernetes/Deployment-v2-timeout.yml -n tutorial
 
 ./../scripts/run.sh http://$(kubectl get route istio-ingressgateway -n istio-system --output 'jsonpath={.status.ingress[].host}')/customer
