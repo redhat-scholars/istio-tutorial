1b38e61ae2f32972376ea109cd28f43207d9e4cd
Stéphane Rondinaud <srondin@rondinaud.fr>
2021-04-04 15:30:39 +0200

    Added Linux support to generate_docs.sh script

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 docker run -v `pwd`:/antora --rm -t antora/antora:2.2.0 --pull --stacktrace site-gh-pages.yml
-open gh-pages/istio-tutorial/1.1.x/index.html
+INDEX_BASE=gh-pages/istio-tutorial/1.6.x/index.html
+case $(uname) in
+  Linux )
+    xdg-open "$INDEX_BASE"
+  ;;
+  Darwin )
+    open "$INDEX_BASE"
+  ;;
+  * )
+  ;;
+esac
