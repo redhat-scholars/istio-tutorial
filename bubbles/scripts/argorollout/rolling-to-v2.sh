8aa1882596d42ad3fccbbb640a170e4bdb384547
Alex Soto <asotobu@gmail.com>
2021-12-15 13:38:20 +0100

    Fixes getting gateway

--- bubbles/scripts/argorollout/rolling-to-v2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f ../../bubbles-backend/src/main/argorollouts/rollout-v2.yaml
--- bubbles/scripts/istio/redirect-to-v2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "###### Redirecting Traffic to V2 ######"
-
-kubectl apply -f ../../bubbles-backend/src/main/istio/virtual-service-bubble-backend-v1_and_v2_0_100.yaml
