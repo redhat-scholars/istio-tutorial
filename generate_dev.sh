92c065e4fcce4ea647a4ccf92751c308ea40dd99
Evan Shortiss <evanshortiss@gmail.com>
2022-12-13 09:16:41 -0800

    Updates to Setup, Deploy, and Observability section (#278)
    * fix: address platform arch warning on M1 mac
    
    * update setup instructions to improve clarity
    
    * fix URL for istio ebook
    
    * change nav title for setup section
    
    * make requirements for openshift section clearer
    
    * add images and more detail to the setup and deploy sections
    
    * update nav and deployment instructions for customer service
    
    * add local devlopment instructions to readme
    
    * update gitignore for downloaded istio binaries
    
    * add 'private:true' to package.json to prevent accidental publication
    
    * improve envoy/sidecar explanation and OC CLI setup sections
    
    * revise and update the observability section
    
    * fix note link to move from deploy to observe section
    
    * add grafana, prometheus, and jaeger nav sections

--- generate_dev.sh
+++ generate_dev.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -v `pwd`:/antora --rm -t antora/antora:2.2.0 --pull --stacktrace dev-site.yml
+docker run -v `pwd`:/antora --platform linux/amd64 --rm -t antora/antora:2.3.4 --pull --stacktrace dev-site.yml
